18df0ee96dc28795e5ce772fcff85800ef4148d4
haibeihai <haibei0408@icloud.com>
2020-07-24 20:08:44 +0800

    客户端直接链接客户端-熔断器

--- product.go
+++ product.go
@@ -1,70 +1,37 @@
 package main
 
 import (
-	"context"
 	"fmt"
-	"io"
-	"net/url"
-	"os"
+	"gomicro2/util"
+	"log"
 	"time"
 
-	"github.com/go-kit/kit/endpoint"
-	"github.com/go-kit/kit/log"
-	"github.com/go-kit/kit/sd"
-
-	"github.com/go-kit/kit/sd/consul"
-	"github.com/go-kit/kit/sd/lb"
-	consulapi "github.com/hashicorp/consul/api"
-
-	"gomicro2/services"
-
-	httptransport "github.com/go-kit/kit/transport/http"
+	"github.com/afex/hystrix-go/hystrix"
 )
 
 func main() {
-	// 第一步： 创建client
-
-	config := consulapi.DefaultConfig()
-	config.Address = "192.168.31.82:8500" // 注册中心地址
-
-	apiClient, _ := consulapi.NewClient(config)
-	client := consul.NewClient(apiClient)
-
-	var logger log.Logger
-	{
-		logger = log.NewLogfmtLogger(os.Stdout)
+	configA := hystrix.CommandConfig{
+		Timeout:                2000,
+		MaxConcurrentRequests:  5,
+		RequestVolumeThreshold: 3,
+		ErrorPercentThreshold:  20,
+		SleepWindow:            int(time.Second * 100),
 	}
-	{
-		tags := []string{"primary", "v1"}
-		// 可实时查询服务实例的状态信息
-		instancer := consul.NewInstancer(client, logger, "userservice", tags, true)
 
-		factory := func(serviceURL string) (endpoint.Endpoint, io.Closer, error) {
-			tat, _ := url.Parse("http://" + serviceURL)
-			return httptransport.NewClient("GET", tat, services.GetUserInfoRequest, services.GetUserInfoResponse).Endpoint(), nil, nil
+	hystrix.ConfigureCommand("getuser", configA)
+	err := hystrix.Do("getuser", func() error {
+		res, err := util.GetUser()
+		if err != nil {
+			return err
 		}
-		endpointer := sd.NewEndpointer(instancer, factory, logger)
-		endpionts, _ := endpointer.Endpoints()
-		fmt.Println("服务有：", len(endpionts), "个")
-		if len(endpionts) == 0 {
-			return
-		}
+		fmt.Println(res)
+		return nil
+	}, func(err error) error {
+		fmt.Println("降级用户")
+		return err
+	})
 
-		//mylb := lb.NewRoundRobin(endpointer)
-		mylb := lb.NewRandom(endpointer, time.Now().UnixNano())
-
-		for {
-			// getUserInfo := endpionts[0] // 写死获取第一个
-			getUserInfo, _ := mylb.Endpoint()
-			ctx := context.Background()
-			res, err := getUserInfo(ctx, services.UserRequest{UID: 101})
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-			userInfo := res.(services.UserResponse)
-			fmt.Println(userInfo.Result)
-			time.Sleep(time.Second * 3)
-		}
+	if err != nil {
+		log.Fatal(err)
 	}
 }
---
+++ util/user_util.go
@@ -0,0 +1,64 @@
+package util
+
+import (
+	"context"
+	"gomicro2/services"
+	"io"
+	"net/url"
+	"os"
+	"time"
+
+	"github.com/go-kit/kit/endpoint"
+	"github.com/go-kit/kit/log"
+	"github.com/go-kit/kit/sd"
+	"github.com/go-kit/kit/sd/consul"
+	"github.com/go-kit/kit/sd/lb"
+
+	httptransport "github.com/go-kit/kit/transport/http"
+
+	consulapi "github.com/hashicorp/consul/api"
+)
+
+func GetUser() (string, error) {
+	// 第一步： 创建client
+	config := consulapi.DefaultConfig()
+	config.Address = "192.168.31.82:8500" // 注册中心地址
+
+	apiClient, err := consulapi.NewClient(config)
+	if err != nil {
+		return "", err
+	}
+	client := consul.NewClient(apiClient)
+
+	logger := log.NewLogfmtLogger(os.Stdout)
+
+	tags := []string{"primary", "v1"}
+	// 可实时查询服务实例的状态信息
+	instancer := consul.NewInstancer(client, logger, "userservice", tags, true)
+
+	factory := func(serviceURL string) (endpoint.Endpoint, io.Closer, error) {
+		tat, _ := url.Parse("http://" + serviceURL)
+		return httptransport.NewClient("GET", tat, services.GetUserInfoRequest, services.GetUserInfoResponse).Endpoint(), nil, nil
+	}
+	endpointer := sd.NewEndpointer(instancer, factory, logger)
+
+	//mylb := lb.NewRoundRobin(endpointer) // 轮询
+	mylb := lb.NewRandom(endpointer, time.Now().UnixNano()) // 随机
+
+	getUserInfo, err := mylb.Endpoint()
+	if err != nil {
+		return "", err
+	}
+
+	ctx := context.Background() // 第三步：创建一个context上下文对象
+
+	// 第四部：执行
+	res, err := getUserInfo(ctx, services.UserRequest{UID: 101})
+	if err != nil {
+		return "", err
+	}
+
+	// 第五步：断言，得到响应值
+	userInfo := res.(services.UserResponse)
+	return userInfo.Result, nil
+}
